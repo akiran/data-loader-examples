9093d51c400c4c57ef08e9bd013602428a2c4b47
kiran <kiran@neostack.com>
2018-01-17 14:10:39 +0530

    dataloader with knex

--- src/examples/mysql-read.js
+++ src/examples/mysql-read.js
@@ -1,14 +1,26 @@
 let knex = require('../knex-init')
+let DataLoader = require('dataloader')
 
-
-async function readUsers() {
- const users = await knex('user').select('*')
- console.log('user data', users)
+function myBatchGetUsers(keys) {
+  console.log('keys', keys)
+  const ids = keys.map(key => key.id)
+  return knex('users').select('*').whereIn('id', ids)
 }
 
-async function run() {
-  await readUsers()
-  knex.destroy()
-}
+let userLoader = new DataLoader(keys => myBatchGetUsers(keys));
 
-run()
+// Loading single items
+userLoader.load({id:1})
+  .then(user => {
+    console.log('user with id 1', user)
+  })
+userLoader.load({id:2})
+  .then(user => {
+    console.log('user with id 2', user)
+  })
+
+// Loading multiple items
+userLoader.loadMany([{id:3}, {id:4}])
+  .then(users => {
+    console.log('user with id 3, 4', users)
+  })
